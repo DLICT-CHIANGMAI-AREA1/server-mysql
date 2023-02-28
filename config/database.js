3fa9c50b2f33806fc510f68f255a34db664cd408
artjitt9611 <artjitt2512@gmail.com>
2023-02-28 09:07:00 +0700

    delete all

--- config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(
-	"mongodb+srv://admin:1234@cluster0.r8jzdpp.mongodb.net/MyFristDatabase?retryWrites=true&w=majority",
-	{ useNewUrlParser: true },
-	
-);
-
-module.exports = mongoose
